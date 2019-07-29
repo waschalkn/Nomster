252c63f8e774c6b80231607169f3b18eb8691353
waschalkn <nacodaw93@gmail.com>
2019-07-29 21:53:00 +0000

    debug

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module Nomster
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    config.assets.initialize_on_precompile = false
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
@@ -20,5 +20,7 @@ module Nomster
 
     # Precompile additional assets
     config.assets.precompile += %w( .svg .eot .woff .ttf )
+
+    config.assets.initialize_on_precompile = false
   end
 end
