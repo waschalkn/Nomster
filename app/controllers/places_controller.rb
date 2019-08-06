65713281a2792c24a6180b90c27534827e2d76a1
waschalkn <nacodaw93@gmail.com>
2019-08-06 22:01:23 +0000

    only user who created place can now destroy

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def index
     @places = Place.order("name").page(params[:page]).per_page(5)
@@ -32,7 +32,7 @@ class PlacesController < ApplicationController
     if @place.user != current_user
       return render plain: 'Not Allowed', status: :forbidden
     end
-    
+
     @place.update_attributes(place_params)
 
     redirect_to root_path
@@ -40,6 +40,8 @@ class PlacesController < ApplicationController
 
   def destroy
     @place = Place.find(params[:id])
+    if @place.user != current_user
+      return render plain: 'Not Allowed', status: :forbidden
     @place.destroy
     redirect_to root_path
   end
