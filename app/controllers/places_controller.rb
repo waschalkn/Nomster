32e34229aa1ba9b066ed48b7ed558f67d40377fc
waschalkn <nacodaw93@gmail.com>
2019-08-06 23:50:10 +0000

    fixed bug

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -42,6 +42,7 @@ class PlacesController < ApplicationController
     @place = Place.find(params[:id])
     if @place.user != current_user
       return render plain: 'Not Allowed', status: :forbidden
+    end
     @place.destroy
     redirect_to root_path
   end
