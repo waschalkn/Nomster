073557497095e3f97bdd347150cc0e8b39c55e52
waschalkn <nacodaw93@gmail.com>
2019-08-07 22:51:55 +0000

    bug fix

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,7 +1,9 @@
 class Place < ApplicationRecord
   belongs_to :user
+  
   geocoded_by :address
-  after_validation :geocode 
+  after_validation :geocode
+
   validates :name, presence: true, length: { maximum: 40, minimum: 3 }
   validates :address, presence: true
   validates :description, presence: true
