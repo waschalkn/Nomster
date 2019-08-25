3c16e10895c436c049b3d5f0da3681fc44baa5ee
waschalkn <nacodaw93@gmail.com>
2019-08-25 19:12:19 +0000

    added current user email, total comments, and total 5 star reviews to user profile page.

--- db/schema.rb
+++ db/schema.rb
@@ -49,7 +49,7 @@ ActiveRecord::Schema.define(version: 2019_08_17_004616) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
+    t.string "email", default: @user, null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
